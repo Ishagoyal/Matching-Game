5949e641e8b49cbeb715a79c4cf0254212757f53
Isha Goyal <iamishagoyal@gmail.com>
2017-02-07 23:34:22 +0530

    click sound added, timer not working

--- MatchingGame.js
+++ MatchingGame.js
@@ -7,12 +7,18 @@ var leftSideImages;
 var theBody;
 var count=0;
 var score;
+var timer;
+var sound;
+var clicked=false;
+
 
 function generateFaces(){
 theLeftSide=document.getElementById("leftSide");
 theRightSide=document.getElementById("rightSide");
 score=document.getElementById("score");
 score.innerHTML=count;
+sound=document.createElement("audio");
+sound.src="http://themezz.com/files/Sounds/Click-5.wav";
 while(numberOfFaces>0){
 var smileImg=document.createElement("img");
 smileImg.src="http://home.cse.ust.hk/~rossiter/mooc/matching_game/smile.png";
@@ -33,7 +39,10 @@ numberOfFaces--;
 leftSideImages=theLeftSide.cloneNode(true);
 leftSideImages.removeChild(leftSideImages.lastChild);
 theRightSide.append(leftSideImages);
+clicked=false;
 theLeftSide.lastChild.onclick=function nextLevel(event){
+clicked=true;
+sound.play();
 theLeftSide.removeChild(theLeftSide.lastChild);
  event.stopPropagation();
  numberOfFaces += 4;
@@ -45,13 +54,26 @@ theLeftSide.removeChild(theLeftSide.lastChild);
 
 theBody=document.getElementsByTagName("body")[0];
 
-theBody.onclick=function gameOver(){
+theBody.onclick= function gameOver(){
+sound.play();
 alert("Game Over! You have not clicked on the extra smiley...\nAnd Your Score is " +count+ "!");
 theBody.onclick=null;
 theLeftSide.lastChild.onclick=null;
 window.location.reload();
 
 }
+function timeOver(){
+ alert("Time Up! Game Over!\n And Your Score is "+count+ "!");
+ theBody.onclick=null;
+theLeftSide.lastChild.onclick=null;
+window.location.reload();
+}
+
+if(clicked==false){
+timer=setTimeout(timeOver, 10000);
+console.log(clicked);
+}
+
 
 }
 
